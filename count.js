d796c7896cb3dbf7f667f5e5a7bc698050b5b6b0
jacobcarterMATH <72286392+jacobcarterMATH@users.noreply.github.com>
2022-02-07 12:15:46 -0500

    counter saves between pages
    counter now saves the count value to session storage so the counter is saved if the user navigates to another page

--- count.js
+++ count.js
@@ -1,13 +1,21 @@
-	let count = 0;
-	var display = document.getElementById('display');
-	function increment(){
-		count = count + 1;
-		document.getElementById('incrementButton').innerHTML = "Runs: " + count;
-	}
+var count = 0;
+if (!sessionStorage.getItem("count")){
+	count = 0;
+	sessionStorage.setItem("count", count);
+} else {
+	count = parseInt(sessionStorage.getItem("count"));
+}
+
+function increment(){
+	count = count + 1;
+	sessionStorage.setItem("count", count);
+	document.getElementById('incrementButton').innerHTML = "Runs: " + sessionStorage.getItem("count");
+}
 	
-	function reset(){
-		if (confirm('This cannot be undone - are you sure?')){
-			count = 0;
-		}
-		document.getElementById('incrementButton').innerHTML = "Runs: " + count;
+function reset(){
+	if (confirm('This cannot be undone - are you sure?')){
+		count = 0;
+		sessionStorage.setItem("count", count);
 	}
+	document.getElementById('incrementButton').innerHTML = "Runs: " + sessionStorage.getItem("count");
+}
